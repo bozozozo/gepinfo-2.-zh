ac1cd25fd22035fe9c9386499eec80ed9a92c720
bozozozo <zotya2525@gmail.com>
2024-05-30 17:51:26 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(30, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(367, 272);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(519, 28);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 279);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(647, 32);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 279);
+            this.trackBar2.TabIndex = 2;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(516, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(516, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(644, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(187, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 49);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(314, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 49);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(464, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 49);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(600, 378);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 49);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(30, 290);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(170, 290);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(126, 47);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(414, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(84, 277);
+            this.listBox1.TabIndex = 14;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int i = 0; 
+        bool forward = true; 
+        int fastcounter = 0;
+        int number_of_slides;
+        List<Image> images = new List<Image>();
+        bool hide_mode = false;
+        bool atlatszo = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            atlatszo = !atlatszo;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = 1;    
+            trackBar1.Maximum = 1500; 
+            trackBar1.Value = 100;      
+            trackBar2.Minimum = 0;
+            trackBar2.Maximum = 1;
+            trackBar2.LargeChange = 1;  
+            label1.Text = "Speed";
+            label2.Text = "# of Pic";
+            label3.Text = "100 ms";
+            label4.Text = "";
+            this.Text = "Animation Player";  
+            button1.Text = "Start";
+            button2.Text = "Forward/Reverse";
+            button3.Text = "Fast Play";
+            button4.Text = "Load Images";
+            button5.Text = "Pic Effects";
+            button6.Text = "Picture on Picture";
+            timer1.Enabled = false;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatAppearance.MouseOverBackColor = Color.FromArgb(50, Color.Black);
+            comboBox1.Text = "Válasszon effektust!";
+
+            comboBox1.Items.AddRange(new string[] { "horizontális flipp", "vertikális flipp", "90 fok(-os pálinka hatása)", "szempöl", "picibb", "vÍzes márk" });
+            comboBox1.SelectedIndex = -1; 
+            comboBox1.Enabled = false; 
+            comboBox1.Visible = false;
+            button6.Visible = false;
+
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            number_of_slides = images.Count;
+            if (number_of_slides == 0) return;
+            timer1.Interval = trackBar1.Value;
+            pictureBox1.Image = images[i];
+            listBox1.SelectedIndex = i;
+            if (forward)
+                i = (i + 1) % number_of_slides;
+            else
+            {
+                i = (i + number_of_slides - 1) % number_of_slides;
+                if (i == 0) i = number_of_slides - 1;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+                button1.Text = "Start";
+            else
+                button1.Text = "Stop";
+            timer1.Enabled = !timer1.Enabled;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            forward = !forward;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            switch (fastcounter)
+            {
+                case 0:
+                    button3.Text = "Fast Play 2x";
+                    timer1.Interval /= 2;
+                    break;
+                case 1:
+                    button3.Text = "Fast Play 4x";
+                    timer1.Interval /= 2;
+                    break;
+                case 2:
+                    button3.Text = "Fast Play 8x";
+                    timer1.Interval /= 2;
+                    break;
+                case 3:
+                    button3.Text = "Normal Play";
+                    timer1.Interval = 200;
+                    break;
+            }
+            fastcounter = (fastcounter + 1) % 4;
+            trackBar1.Value = timer1.Interval;
+            label3.Text = trackBar1.Value.ToString() + " ms";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            images.Clear();
+            pictureBox1.Image = null;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string szijjarto = folderBrowserDialog1.SelectedPath, fn;
+                foreach (string fullpathfn in Directory.GetFiles(szijjarto))
+                {
+                    Image img = Image.FromFile(fullpathfn);
+                    images.Add(img);
+
+                    fn = Path.GetFileName(fullpathfn); 
+                    listBox1.Items.Add(fn);            
+                }
+                trackBar2.Minimum = 1;               
+                trackBar2.Maximum = listBox1.Items.Count;
+
+                
+                comboBox1.Enabled = true;
+            }
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button5.Visible = true;
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int si = listBox1.SelectedIndex;
+            pictureBox1.Image = images[si];
+            trackBar2.Value = si + 1;
+            label4.Text = trackBar2.Value.ToString() + ". pic";
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label3.Text = trackBar1.Value.ToString() + " msec";
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label4.Text = trackBar2.Value.ToString() + ". pic";
+            listBox1.SelectedIndex = trackBar2.Value - 1;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            comboBox1.Visible = true;
+            button6.Visible = true;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(images[listBox1.SelectedIndex]);
+            Graphics g = Graphics.FromImage(bmp);
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: 
+                    bmp.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    break;
+                case 1: 
+                    bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                    break;
+                case 2: 
+                    bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    break;
+                case 3: 
+                    g.DrawString("szempöl tekszt", new Font("Arial", 20), Brushes.Red, new PointF(10, 10));
+                    break;
+                case 4: 
+                    bmp = new Bitmap(bmp, new Size(bmp.Width / 2, bmp.Height / 2));
+                    break;
+                case 5: 
+                    g.DrawString("Vótör Márk", new Font("Arial", 20), Brushes.Blue, new PointF(bmp.Width - 100, bmp.Height - 30));
+                    break;
+            }
+
+            g.Dispose();
+            images[listBox1.SelectedIndex] = bmp;
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = this.CreateGraphics())
+            {
+                Rectangle rect = new Rectangle(pictureBox1.Left - 5, pictureBox1.Top - 5, pictureBox1.Width + 10, pictureBox1.Height + 10);
+                using (Pen pen = new Pen(Color.Green, 3))
+                {
+                    g.DrawRectangle(pen, rect);
+                }
+            }
+        }
+    }
+}
